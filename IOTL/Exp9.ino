b7bb21e13ca356e57fc98bbd18cc6d5e328a000c
Sudhir <sudhirsingh3923@gmail.com>
2025-05-07 22:58:42 +0530

    modify the program 9

--- IOTL/Exp9.ino
+++ IOTL/Exp9.ino
@@ -24,3 +24,46 @@ void loop() {
   
   delay(100);
 }
+
+/*
+Problem Statement: 
+To change the color of the RGB LED using the input from 3 different potentiometers, using 
+Arduino. 
+Code: 
+const int rPin = 9;      
+// PWM pin for Red color 
+const int gPin = 10;   // PWM pin for Green color 
+const int bPin = 11;   // PWM pin for Blue color 
+const int rPotPin = A0;  // Analog input pin for Red potentiometer 
+const int gPotPin = A1;  // Analog input pin for Green potentiometer 
+const int bPotPin = A2;  // Analog input pin for Blue potentiometer 
+void setup() { 
+// Set RGB LED pins as output 
+pinMode(rPin, OUTPUT); 
+pinMode(gPin, OUTPUT); 
+pinMode(bPin, OUTPUT); 
+Serial.begin(9600); 
+} 
+void loop() { 
+// Read the potentiometer values 
+int rValue = analogRead(rPotPin); 
+int gValue = analogRead(gPotPin); 
+int bValue = analogRead(bPotPin); 
+// Map the potentiometer values to a range of 0 to 255 (PWM range) 
+rValue = map(rValue, 0, 1023, 0, 255); 
+gValue = map(gValue, 0, 1023, 0, 255); 
+bValue = map(bValue, 0, 1023, 0, 255); 
+// Set the color of the RGB LED using PWM 
+analogWrite(rPin, rValue); 
+analogWrite(gPin, gValue); 
+analogWrite(bPin, bValue); 
+// Print values to Serial Monitor 
+Serial.print("\n\nRed: "); 
+Serial.print(rValue); 
+Serial.print("  Green: "); 
+Serial.print(gValue); 
+Serial.print("  Blue: "); 
+Serial.print(bValue); 
+delay(50); // Small delay for stability 
+}
+*/
